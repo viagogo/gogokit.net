80895701cf38ba1ddb9bf526da9a8a988d3dc725
Akil Burgess <akil.burgess@viagogo.com>
2014-11-25 16:38:23 +0000

    Initial commit - here we go!

---
+++ README.md
@@ -0,0 +1,3 @@
+# Viagogo.Sdk - viagogo API Client Library for .NET
+
+Viagogo.Sdk is a lightweight async viagogo API client library for .NET.
---
+++ Viagogo.Sdk/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Viagogo.Sdk")]
+[assembly: AssemblyProduct("Viagogo.Sdk")]
+[assembly: AssemblyDescription("A lightweight async viagogo API client library for .NET")]
+
+[assembly: AssemblyCompany("viagogo")]
+[assembly: AssemblyCopyright("Copyright © viagogo 2014")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]

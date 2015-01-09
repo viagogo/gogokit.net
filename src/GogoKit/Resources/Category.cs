752f85539f5a7f9b35b81afca280e9cb9843fdf1
Michali Konstantinidis <michali.konstantinidis@viagogo.com>
2015-01-09 17:34:28 +0000

    Removed uneccesary Order Property

--- src/GogoKit/Resources/Category.cs
+++ src/GogoKit/Resources/Category.cs
@@ -7,19 +7,19 @@ namespace GogoKit.Resources
     [DataContract]
     public class Category : Resource
     {
-        [DataMember(Name = "id", Order = 100)]
+        [DataMember(Name = "id")]
         public int Id { get; set; }
 
-        [DataMember(Name = "description_html", Order = 100)]
+        [DataMember(Name = "description_html")]
         public string Description { get; set; }
 
-        [DataMember(Name = "min_ticket_price", Order = 104)]
+        [DataMember(Name = "min_ticket_price")]
         public Money MinTicketPrice { get; set; }
 
-        [DataMember(Name = "min_event_date", Order = 105)]
+        [DataMember(Name = "min_event_date")]
         public DateTimeOffset? MinEventDate { get; set; }
 
-        [DataMember(Name = "max_event_date", Order = 106)]
+        [DataMember(Name = "max_event_date")]
         public DateTimeOffset? MaxEventDate { get; set; }
     }
 }

00166cac871a2814316aa0213e20f3669a023cb3
Akil Burgess <akilburgess@gmail.com>
2015-04-02 12:32:03 +0100

    Added project and license urls to nuspec

--- build.fsx
+++ build.fsx
@@ -10,7 +10,7 @@ open Fake.XUnit2Helper
 let projectName = "GogoKit"
 let projectDescription = "A viagogo API client library for .NET"
 let authors = ["viagogo"]
-let copyright = @"Copyright © viagogo " + DateTime.UtcNow.ToString("yyyy");
+let copyright = @"Copyright viagogo " + DateTime.UtcNow.ToString("yyyy");
 
 // Directories
 let buildDir = @"./artifacts/"

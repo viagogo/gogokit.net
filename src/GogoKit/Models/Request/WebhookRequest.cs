d343dd2bbf6e36ada0ca7a2e0c8a59aae4530443
Akil Burgess <akilburgess@gmail.com>
2015-10-12 12:24:45 +0100

    Add webhook request and response models

---
+++ src/GogoKit/Models/Request/WebhookRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace GogoKit.Models.Request
+{
+    public class WebhookRequest : RequestParameters<WebhookEmbed, WebhookSort>
+    {
+        protected override IDictionary<WebhookSort, string> SortFieldNameMap
+        {
+            get
+            {
+                return new Dictionary<WebhookSort, string>
+                {
+                    {WebhookSort.CreatedAt, "created_at"}
+                };
+            }
+        }
+    }
+
+    public enum WebhookEmbed
+    {
+    }
+
+    public enum WebhookSort
+    {
+        CreatedAt
+    }
+}
---
+++ src/GogoKit/Models/Response/Webhook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using HalKit.Models.Response;
+
+namespace GogoKit.Models.Response
+{
+    [DataContract]
+    public class Webhook : Resource
+    {
+        [DataMember(Name = "id")]
+        public int? Id { get; set; }
+
+        [DataMember(Name = "created_at")]
+        public DateTimeOffset? CreatedAt { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "url")]
+        public string Url { get; set; }
+
+        [DataMember(Name = "topics")]
+        public IList<string> Topics { get; set; }
+    }
+}

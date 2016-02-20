e8a01864325e8a4a2a0fa020e22bce442993b2e2
Akil Burgess <akilburgess@gmail.com>
2016-02-20 13:28:28 +0000

    Add AuthorizationEndpoint and Docs to config
    * Added ViagogoAuthorizationEndpoint property to IGogoKitConfiguration
    * Added XML doc comments for all of the configuration properties

--- src/GogoKit/GogoKitConfiguration.cs
+++ src/GogoKit/GogoKitConfiguration.cs
@@ -1,46 +1,54 @@
 ﻿using System;
 using GogoKit.Enumerations;
 using HalKit;
-using System.Collections.Generic;
+using System.Threading;
 
 namespace GogoKit
 {
     public class GogoKitConfiguration : IGogoKitConfiguration
     {
-        private static readonly IDictionary<ApiEnvironment, Uri> DefaultViagogoApiRootEndpoints =
-            new Dictionary<ApiEnvironment, Uri>()
-            {
-                [ApiEnvironment.Production] = new Uri("https://api.viagogo.net/v2"),
-                [ApiEnvironment.Sandbox] = new Uri("https://sandbox.api.viagogo.net/v2")
-            };
-
-        private static readonly IDictionary<ApiEnvironment, Uri> DefaultViagogoOAuthTokenEndpoints =
-            new Dictionary<ApiEnvironment, Uri>()
-            {
-                [ApiEnvironment.Production] = new Uri("https://www.viagogo.com/secure/oauth2/token"),
-                [ApiEnvironment.Sandbox] = new Uri("https://sandbox.api.viagogo.net/v2")
-            };
-
         private readonly HalKitConfiguration _halKitConfiguration;
         private ApiEnvironment _apiEnvironment;
 
         public GogoKitConfiguration()
         {
-            _halKitConfiguration = new HalKitConfiguration(DefaultViagogoApiRootEndpoints[ApiEnvironment.Production])
+            _halKitConfiguration = new HalKitConfiguration(Default.ViagogoApiRootEndpoints[ApiEnvironment.Production])
                                    {
                                        CaptureSynchronizationContext = false
                                    };
             ViagogoApiEnvironment = ApiEnvironment.Production;
         }
 
+        /// <summary>
+        /// The root endpoint of the API to get the root resource that links to
+        /// all other API resources.
+        /// </summary>
+        /// <remarks>See http://developer.viagogo.net/#explorable-api</remarks>
         public Uri ViagogoApiRootEndpoint
         {
             get { return _halKitConfiguration.RootEndpoint; }
             set { _halKitConfiguration.RootEndpoint = value; }
         }
 
+        /// <summary>
+        /// The endpoint where OAuth2 access tokens are granted.
+        /// </summary>
+        /// <remarks>See http://developer.viagogo.net/#getting-access-tokens</remarks>
         public Uri ViagogoOAuthTokenEndpoint { get; set; }
 
+        /// <summary>
+        /// The endpoint where you can obtain a user’s consent to make API calls
+        /// on their behalf.
+        /// </summary>
+        /// <remarks>See http://developer.viagogo.net/#authorization-code-grant</remarks>
+        public Uri ViagogoAuthorizationEndpoint { get; set; }
+
+        /// <summary>
+        /// Determines which <see cref="ApiEnvironment"/> should be used. Setting
+        /// this value will configure the values for <see cref="ViagogoApiRootEndpoint"/>,
+        /// <see cref="ViagogoOAuthTokenEndpoint"/> and <see cref="ViagogoAuthorizationEndpoint"/>.
+        /// </summary>
+        /// <remarks>See http://developer.viagogo.net/#sandbox-environment</remarks>
         public ApiEnvironment ViagogoApiEnvironment
         {
             get
@@ -50,21 +58,40 @@ namespace GogoKit
             set
             {
                 _apiEnvironment = value;
-                ViagogoApiRootEndpoint = DefaultViagogoApiRootEndpoints[_apiEnvironment];
-                ViagogoOAuthTokenEndpoint = DefaultViagogoOAuthTokenEndpoints[_apiEnvironment];
+                ViagogoApiRootEndpoint = Default.ViagogoApiRootEndpoints[_apiEnvironment];
+                ViagogoOAuthTokenEndpoint = Default.ViagogoOAuthTokenEndpoints[_apiEnvironment];
+                ViagogoAuthorizationEndpoint = Default.ViagogoAuthorizationEndpoints[_apiEnvironment];
             }
         }
 
+        /// <summary>
+        /// Determines whether asynchronous operations should capture the current
+        /// <see cref="SynchronizationContext"/>.
+        /// </summary>
+        /// <remarks>See http://blog.stephencleary.com/2012/02/async-and-await.html#avoiding-context.</remarks>
         public bool CaptureSynchronizationContext
         {
             get { return _halKitConfiguration.CaptureSynchronizationContext; }
             set { _halKitConfiguration.CaptureSynchronizationContext = value; }
         }
 
+        /// <summary>
+        /// Determines the language of the API response content (e.g. event names
+        /// and error messages).
+        /// </summary>
+        /// <remarks>See http://developer.viagogo.net/#localization</remarks>
         public string LanguageCode { get; set; }
 
+        /// <summary>
+        /// Determines the geography-context of requests.
+        /// </summary>
+        /// <remarks>See http://developer.viagogo.net/#localization</remarks>
         public string CountryCode { get; set; }
 
+        /// <summary>
+        /// Determines the currency of responses that include monetary values.
+        /// </summary>
+        /// <remarks>See http://developer.viagogo.net/#localization</remarks>
         public string CurrencyCode { get; set; }
     }
 }
--- src/GogoKit/IGogoKitConfiguration.cs
+++ src/GogoKit/IGogoKitConfiguration.cs
@@ -6,22 +6,51 @@ namespace GogoKit
 {
     public interface IGogoKitConfiguration
     {
+        /// <summary>
+        /// The root endpoint of the API to get the root resource that links to
+        /// all other API resources.
+        /// </summary>
+        /// <remarks>See http://developer.viagogo.net/#explorable-api</remarks>
         Uri ViagogoApiRootEndpoint { get; set; }
 
+        /// <summary>
+        /// The endpoint where OAuth2 access tokens are granted.
+        /// </summary>
+        /// <remarks>See http://developer.viagogo.net/#getting-access-tokens</remarks>
         Uri ViagogoOAuthTokenEndpoint { get; set; }
 
         /// <summary>
+        /// The endpoint where you can obtain a user’s consent to make API calls
+        /// on their behalf.
+        /// </summary>
+        /// <remarks>See http://developer.viagogo.net/#authorization-code-grant</remarks>
+        Uri ViagogoAuthorizationEndpoint { get; set; }
+
+        /// <summary>
         /// Determines which <see cref="ApiEnvironment"/> should be used. Setting
-        /// this will value overrides the values for <see cref="ViagogoApiRootEndpoint"/>
-        /// and <see cref="ViagogoOAuthTokenEndpoint"/>.
+        /// this value will configure the values for <see cref="ViagogoApiRootEndpoint"/>,
+        /// <see cref="ViagogoOAuthTokenEndpoint"/> and <see cref="ViagogoAuthorizationEndpoint"/>.
         /// </summary>
         /// <remarks>See http://developer.viagogo.net/#sandbox-environment</remarks>
         ApiEnvironment ViagogoApiEnvironment { get; set; }
 
+        /// <summary>
+        /// Determines the language of the API response content (e.g. event names
+        /// and error messages).
+        /// </summary>
+        /// <remarks>See http://developer.viagogo.net/#localization</remarks>
         string LanguageCode { get; set; }
 
+        /// <summary>
+        /// Determines the geography-context of requests.
+        /// </summary>
+        /// <remarks>See http://developer.viagogo.net/#localization</remarks>
         string CountryCode { get; set; }
 
+        /// <summary>
+        /// Determines the currency of responses that include monetary values.
+        /// </summary>
+        /// <remarks>See http://developer.viagogo.net/#localization</remarks>
         string CurrencyCode { get; set; }
 
         /// <summary>

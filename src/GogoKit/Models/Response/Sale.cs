3d8945b4981845ff37a69545abe1264a7f6ad9f2
Akil Burgess <akilburgess@gmail.com>
2016-02-21 16:42:58 +0000

    Add missing Sale properties

--- src/GogoKit/Models/Response/Sale.cs
+++ src/GogoKit/Models/Response/Sale.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Runtime.Serialization;
 using HalKit.Models.Response;
+using HalKit.Json;
 
 namespace GogoKit.Models.Response
 {
@@ -11,7 +12,7 @@ namespace GogoKit.Models.Response
         public int? Id { get; set; }
 
         [DataMember(Name = "created_at")]
-        public DateTime? CreatedAt { get; set; }
+        public DateTimeOffset? CreatedAt { get; set; }
 
         [DataMember(Name = "seating")]
         public Seating Seating { get; set; }
@@ -28,7 +29,51 @@ namespace GogoKit.Models.Response
         [DataMember(Name = "status_description")]
         public string StatusDescription { get; set; }
 
-        [DataMember(Name = "delivery_method")]
-        public string DeliveryMethod { get; set; }
+        [DataMember(Name = "confirm_by")]
+        public DateTimeOffset? ConfirmBy { get; set; }
+
+        [DataMember(Name = "ship_by")]
+        public DateTimeOffset? ShipBy { get; set; }
+
+        [Rel("sale:autotrackshipment")]
+        public Link AutotrackShipmentLink { get; set; }
+
+
+        [Rel("sale:confirm")]
+        public Link ConfirmLink { get; set; }
+
+
+        [Rel("sale:etickets")]
+        public Link ETicketsLink { get; set; }
+
+        [Rel("sale:eticketuploads")]
+        public Link ETicketUploadsLink { get; set; }
+
+        [Rel("sale:reject")]
+        public Link RejectLink { get; set; }
+
+        [Rel("sale:saveetickets")]
+        public Link SaveETicketsLink { get; set; }
+
+        [Rel("sale:shipments")]
+        public Link ShipmentsLink { get; set; }
+
+        [Rel("sale:trackshipment")]
+        public Link TrackShipmentLink { get; set; }
+
+        [Rel("sale:uploadetickets")]
+        public Link UploadETicketsLink { get; set; }
+
+        [Embedded("event")]
+        public Event Event { get; set; }
+
+        [Embedded("venue")]
+        public Venue Venue { get; set; }
+
+        [Embedded("ticket_type")]
+        public TicketType TicketType { get; set; }
+
+        [Embedded("delivery_method")]
+        public DeliveryMethod DeliveryMethod { get; set; }
     }
 }

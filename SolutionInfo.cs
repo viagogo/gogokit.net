de619fba9013c67b1330cd60a6a794bc15aa8cca
Akil Burgess <akilburgess@gmail.com>
2016-03-01 23:42:49 +0000

    Bump version to 0.13.0

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProductAttribute("GogoKit")]
 [assembly: AssemblyCompanyAttribute("viagogo")]
 [assembly: AssemblyCopyrightAttribute("Copyright viagogo 2016")]
-[assembly: AssemblyVersionAttribute("0.12.1")]
-[assembly: AssemblyFileVersionAttribute("0.12.1")]
-[assembly: AssemblyInformationalVersionAttribute("0.12.1")]
+[assembly: AssemblyVersionAttribute("0.13.0")]
+[assembly: AssemblyFileVersionAttribute("0.13.0")]
+[assembly: AssemblyInformationalVersionAttribute("0.13.0")]
 [assembly: ComVisibleAttribute(false)]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.12.1";
+        internal const string Version = "0.13.0";
     }
 }

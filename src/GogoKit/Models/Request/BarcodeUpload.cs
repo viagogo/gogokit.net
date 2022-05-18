b8fcf0856887f59cdd8f2387259e5d1887f022ec
Maxime Nanouche <Maxime.Nanouche@viagogo.com>
2022-05-18 16:46:43 -0400

    Add legacy order bit and update sale and listing models

--- src/GogoKit/Models/Request/BarcodeUpload.cs
+++ src/GogoKit/Models/Request/BarcodeUpload.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.Serialization;
-using System.Text;
+﻿using System.Runtime.Serialization;
 
 namespace GogoKit.Models.Request
 {
-    [DataContract(Name = "barcode")]
-    public class BarcodeUpload
+    [DataContract(Name = "base_barcode")]
+    public class BaseBarcodeUpload
     {
         [DataMember(Name = "seat_ordinal")]
         public int? SeatOrdinal { get; set; }
@@ -16,8 +13,26 @@ namespace GogoKit.Models.Request
 
         [DataMember(Name = "row")]
         public string Row { get; set; }
+    }
 
+    [DataContract(Name = "barcode")]
+    public class BarcodeUpload : BaseBarcodeUpload
+    {
         [DataMember(Name = "barcode_values")]
         public string[] Values { get; set; }
+    }
+
+    [DataContract(Name = "barcode")]
+    public class SellerListingBarcodeUpload : BaseBarcodeUpload
+    {
+        [DataMember(Name = "barcode_values_sha256_hashed")]
+        public string[] BarcodeValuesSha256Hashed { get; set; }
+    }
+
+    [DataContract(Name = "barcode")]
+    public class SaleBarcodeUpload : BaseBarcodeUpload
+    {
+        [DataMember(Name = "barcode_values")]
+        public string[] BarcodeValues { get; set; }
     }
 }

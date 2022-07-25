e29d0b8813f7908df2ac0461dfdbb11d4fc66c3c
Shi Shu <shi.shu@stubhub.com>
2022-07-25 05:14:51 -0700

    Add Seating Mapping Status to seating

--- src/GogoKit/Models/Response/Seating.cs
+++ src/GogoKit/Models/Response/Seating.cs
@@ -16,5 +16,26 @@ namespace GogoKit.Models.Response
 
         [DataMember(Name = "seat_to")]
         public string SeatTo { get; set; }
+
+        [DataMember(Name = "mapping_status")]
+        public MappingStatus MappingStatus { get; set; }
+    }
+
+    public class MappingStatus
+    {
+        [DataMember(Name = "status")]
+        public MappingStatusEnum Status { get; set; }
+
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+    }
+
+    public enum MappingStatusEnum
+    {
+        Unknown = 0,
+        Mapped = 1,
+        Unmapped = 2,
+        Ignored = 3,
+        Rejected = 4
     }
 }

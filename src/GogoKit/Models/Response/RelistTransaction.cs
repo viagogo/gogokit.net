ccbc7a7b4a8f61db5453af3f22684548e2ba3bda
Dylan McNamara <dylan.mcnamara@stubhub.com>
2023-08-04 17:38:36 -0700

    feat[listing]: relist_transaction in get and post APIs for seller listing

---
+++ src/GogoKit/Models/Response/RelistTransaction.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace GogoKit.Models.Response
+{
+    [DataContract(Name = "relist_transaction")]
+    public class RelistTransaction
+    {
+        /// <summary>
+        /// The Id of the transaction that this listing is created from
+        /// </summary>
+        [DataMember(Name = "id")]
+        public int? Id { get; set; }
+    }
+}
